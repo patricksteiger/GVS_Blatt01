451d846be9a04d22be2c90c352029d061177bb31
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-11-13 15:47:28 +0100

    Kleine Verbesserungen.

--- src/main/java/aufgabe2/Aufgabe2.java
+++ src/main/java/aufgabe2/Aufgabe2.java
@@ -12,12 +12,13 @@ public class Aufgabe2 {
         try (ZContext context = new ZContext()) {
             // Erstelle Request-Socket um mit Response-Socket im Server zu kommunizieren.
             ZMQ.Socket socket = context.createSocket(SocketType.REQ);
+            socket.setIPv6(true);
             socket.connect(address);
 
             socket.send(message.getBytes(ZMQ.CHARSET));
             System.out.println("Gesendet: " + message);
 
-            byte[] reply = socket.recv(0);
+            byte[] reply = socket.recv();
             String answer = new String(reply, ZMQ.CHARSET);
             System.out.println("Erhalten: " + answer);
         }
