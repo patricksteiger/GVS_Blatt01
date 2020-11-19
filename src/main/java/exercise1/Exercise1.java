660686f86a2ed7ec910a8ccb275d927380d96152
Patrick Steiger <patrick.steiger@uni-ulm.de>
2020-11-19 15:16:39 +0100

    Small refactor.

--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -1,4 +1,4 @@
-package aufgabe1;
+package exercise1;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,26 +7,38 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Aufgabe1 {
+public class Exercise1 {
     public static void main(String[] args) {
         final String hostName = "gvs.lxd-vs.uni-ulm.de";
         final int port = 3211;
         final String message = "Echo test";
 
+        Socket socket = null;
         try {
-            Socket socket = new Socket(hostName, port);
-
+            socket = new Socket(hostName, port);
+            // Send message
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
             out.println(message);
-            System.out.println("Gesendet: " + message);
-
+            System.out.println("Message: " + message);
+            // Read and print answer
             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String answer = in.readLine();
-            System.out.println("Erhalten: " + answer);
+            System.out.println("Answer : " + answer);
         } catch (UnknownHostException e) {
             System.err.println("Couldn't create IP of server!");
+            e.printStackTrace();
         } catch (IOException e) {
             System.err.println("Couldn't create socket or send or write message to/from server!");
+            e.printStackTrace();
+        } finally {
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    System.out.println("Coudln't close socket!");
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/main/java/exercise2/Exercise2.java
+++ src/main/java/exercise2/Exercise2.java
@@ -1,26 +1,25 @@
-package aufgabe2;
+package exercise2;
 
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
-public class Aufgabe2 {
+public class Exercise2 {
     public static void main(String[] args) {
         final String address = "tcp://gvs.lxd-vs.uni-ulm.de:27347";
         final String message = "Echo test";
 
         try (ZContext context = new ZContext()) {
-            // Erstelle Request-Socket um mit Response-Socket im Server zu kommunizieren.
             ZMQ.Socket socket = context.createSocket(SocketType.REQ);
             socket.setIPv6(true);
             socket.connect(address);
-
+            // Send message
             socket.send(message.getBytes(ZMQ.CHARSET));
-            System.out.println("Gesendet: " + message);
-
+            System.out.println("Message: " + message);
+            // Read and print answer
             byte[] reply = socket.recv();
             String answer = new String(reply, ZMQ.CHARSET);
-            System.out.println("Erhalten: " + answer);
+            System.out.println("Answer : " + answer);
         }
     }
 }
